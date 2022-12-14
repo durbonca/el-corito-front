197a39c783acd6b288a03403cc2326780bf9d295
durbonca <durbonca@gmail.com>
2022-12-14 15:02:08 -0300

    Update index.jsx

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,26 +1,47 @@
 import { PaginationContainer, NavLinks, PageNav } from "./pagination.styles";
 
 export const Pagination = ({ page, totalPages, setPage }) => {
+  const handlePageChange = args => {
+    window.scrollTo(0, 0);
+    setPage(args);
+  };
+
   return (
     <PaginationContainer>
       <NavLinks>
         {page !== 1 && (
           <>
-            <PageNav onClick={() => setPage(page - 1)}>Anterior</PageNav>
-            <PageNav onClick={() => setPage(page - 1)}>{page - 1}</PageNav>
+            <PageNav onClick={() => handlePageChange(page - 1)}>
+              Anterior
+            </PageNav>
+            {page > 2 && (
+              <>
+                <PageNav onClick={() => handlePageChange(1)}>{1}</PageNav>
+                {" ... "}
+              </>
+            )}
+            <PageNav onClick={() => handlePageChange(page - 1)}>
+              {page - 1}
+            </PageNav>
           </>
         )}
         <PageNav active>{page}</PageNav>
         {page < totalPages - 1 && (
           <>
-            <PageNav onClick={() => setPage(page + 1)}>{page + 1}</PageNav>
+            <PageNav onClick={() => handlePageChange(page + 1)}>
+              {page + 1}
+            </PageNav>
             {" ... "}
           </>
         )}
         {page !== totalPages && (
           <>
-            <PageNav onClick={() => setPage(totalPages)}>{totalPages}</PageNav>
-            <PageNav onClick={() => setPage(page + 1)}>Siguiente</PageNav>
+            <PageNav onClick={() => handlePageChange(totalPages)}>
+              {totalPages}
+            </PageNav>
+            <PageNav onClick={() => handlePageChange(page + 1)}>
+              Siguiente
+            </PageNav>
           </>
         )}
       </NavLinks>
