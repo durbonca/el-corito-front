bb665cdd5bde884488bd57c16dc0e8167373ebf4
durbonca <durbonca@gmail.com>
2022-12-15 09:54:51 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-/* import App from "./App"; */
-import { Home } from "./pages/";
 import { NavBar, Footer } from "./components";
 import reportWebVitals from "./reportWebVitals";
 import { router } from "./router";
